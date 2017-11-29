4713d1e02b9aee4c52c64c677079298e5a781ddf
Marcos A. de S. Silva <chuckymass@gmail.com>
2017-11-29 18:00:54 -0300

    Layout básico com thumbnails e título do vídeo

--- search.js
+++ search.js
@@ -1,17 +1,28 @@
-function search() {
-   	var url, q = $('#query').val(), len, i;
-    url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=' + q + '&key=AIzaSyC8XzaFTF3rAW1q_58Fi2majFEyu1smzUY';
-  	$.get(url,
-     	function (r) {
-            var $image, items = r.items, image;
-            len = items.length;
-            $("#search-container").empty();
-            for (i = 0; i < len; i++) {
-                $image = $("<img>");
-                image = items[i].snippet.thumbnails.medium;
-                $image.attr({src: image.url, height: image.height, width: image.width});
-                $("#search-container").append($image);
+$(document).ready(function () {
+    function search() {
+        var url, q = $('#query').val(), len, i, template;
+        url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=' + q + '&key=AIzaSyC8XzaFTF3rAW1q_58Fi2majFEyu1smzUY';
+        $.get(url,
+            function (r) {
+                var $image, $card, items = r.items, image, snippet, $cardImage, $cardContent, $play;
+                len = items.length;
+                $('#search-container').empty();
+                for (i = 0; i < len; i++) {
+                    $play = $('<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>');
+                    $image = $('<img></img>');
+                    $card = $('<div class="card"></div>');
+                    $cardImage = $('<div class="card-image"><div>');
+                    $cardContent = $('<div class="card-content"></div>');
+                    snippet = items[i].snippet;
+                    image = snippet.thumbnails.medium;
+                    $image.attr({src: image.url, height: image.height, width: image.width});
+                    $cardImage.append($image).append($play);
+                    $cardContent.append(snippet.title);
+                    $card.append($cardImage).append($cardContent);
+                    $('#search-container').append($('<div class="col s12 m6 l4"></div>').append($card));
+                }
             }
-        }
-    );
-}
+        );
+    }
+    $('form#search').on('submit', search);
+});
