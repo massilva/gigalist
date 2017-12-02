d24ab96df13b1cdc9af09336905c8d8e0aa9d066
Marcos A. de S. Silva <chuckymass@gmail.com>
2017-12-01 22:23:53 -0300

    Permite busca de multiplos artistas

--- search.js
+++ search.js
@@ -1,32 +1,61 @@
+/*global Materialize*/
+/*jslint nomen: true*/
+/*jslint plusplus: true */
 $(document).ready(function () {
+    'use strict';
     $('.modal').modal();
-    function search() {
-        var url, q = $('#query').val(), len, i, template, $preloader = $('#preloader-modal');
-        url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=' + q + '&key=AIzaSyC8XzaFTF3rAW1q_58Fi2majFEyu1smzUY';
-        $preloader.modal('open');
-        $.get(url).done(function (r) {
-          var $image, $card, items = r.items, image, snippet, $cardImage, $cardContent, $play;
-          len = items.length;
-          $('#search-container').empty();
-          for (i = 0; i < len; i++) {
-              $play = $('<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>');
-              $image = $('<img></img>');
-              $card = $('<div class="card"></div>');
-              $cardImage = $('<div class="card-image"><div>');
-              $cardContent = $('<div class="card-content"></div>');
-              snippet = items[i].snippet;
-              image = snippet.thumbnails.medium;
-              $image.attr({src: image.url, height: image.height, width: image.width});
-              $cardImage.append($image).append($play);
-              $cardContent.append(snippet.title);
-              $card.append($cardImage).append($cardContent);
-              $('#search-container').append($('<div class="col s12 m6 l4"></div>').append($card));
-          }
-        }).fail(function (r) {
-          Materialize.toast('<i class="medium material-icons red-text">highlight_off</i> Infelizmente, houve um erro.', 4000);
-        }).always(function () {
-          $preloader.modal('close');
+    function zip(args) {
+        var shortest;
+        shortest = !args.length ? [] : args.reduce(function (a, b) {
+            return a.length < b.length ? a : b;
         });
+        return shortest.map(function (_, i) {
+            return args.map(function (array) { return array[i]; });
+        });
+    }
+    function processResults(results) {
+        var i, j, orderedResults = zip(results), len = orderedResults.length, items, itemsLen, image, snippet, $image, $card, $cardImage, $cardContent, $play;
+        $('#search-container').empty();
+        $('#preloader-modal').modal('close');
+        for (j = 0; j < len; ++j) {
+            items = orderedResults[j];
+            itemsLen = items.length;
+            for (i = 0; i < itemsLen; ++i) {
+                $play = $('<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>');
+                $image = $('<img></img>');
+                $card = $('<div class="card"></div>');
+                $cardImage = $('<div class="card-image"><div>');
+                $cardContent = $('<div class="card-content"></div>');
+                snippet = items[i].snippet;
+                image = snippet.thumbnails.medium;
+                $image.attr({src: image.url, height: image.height, width: image.width});
+                $cardImage.append($image).append($play);
+                $cardContent.append(snippet.title);
+                $card.append($cardImage).append($cardContent);
+                $('#search-container').append($('<div class="col s12 m6 l4"></div>').append($card));
+            }
+        }
+    }
+    function get(queries, i, len, maxResults, results) {
+        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=' + maxResults + '&q=' + encodeURI(queries[i].trim()) + '&key=AIzaSyC8XzaFTF3rAW1q_58Fi2majFEyu1smzUY';
+        $.get(url).done(function (r) {
+            results.push(r.items);
+            i++;
+            if (i < len) {
+                get(queries, i, len, maxResults, results);
+            } else {
+                processResults(results);
+            }
+        }).fail(function () {
+            Materialize.toast('<i class="medium material-icons red-text">highlight_off</i> Infelizmente, houve um erro.', 3000);
+            processResults(results);
+        });
+    }
+    function search() {
+        var queries, q = $('#query').val();
+        queries = q.split(';');
+        $('#preloader-modal').modal('open');
+        get(queries, 0, queries.length, parseInt(50 / queries.length, 10), []);
     }
     $('form#search').on('submit', search);
 });
